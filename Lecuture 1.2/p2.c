48786d1869b9cd0c92f575b38cf9ffbad4250d04
jenishbabariya22 <jenishbabariya58@gmail.com>
2024-03-26 18:04:49 +0530

    first commit

---
+++ Lecuture 1.2/p2.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+
+main(){
+	
+	//half star
+	printf("\n*\t*\t*\t*\t*\t*\t*\t*\t*\t*");
+	printf("\n\n\n*\t*\t*\t*\t*\t*\t*\t*\t*");
+	printf("\n\n\n*\t*\t*\t*\t*\t*\t*\t*");
+	printf("\n\n\n*\t*\t*\t*\t*\t*\t*");
+	printf("\n\n\n*\t*\t*\t*\t*\t*");
+	printf("\n\n\n*\t*\t*\t*\t*");
+	printf("\n\n\n*\t*\t*\t*");
+	printf("\n\n\n*\t*\t*");
+	printf("\n\n\n*\t*");
+	printf("\n\n\n*");
+	
+	
+	//full start diamond
+	printf("\n\n\n\n\n\n\t\t\t\t    *");
+	printf("\n\n\n\t\t\t\t*\t*");
+	printf("\n\n\n\t\t\t*\t*\t*\t*");
+	printf("\n\n\n\t\t*\t*\t*\t*\t*\t*");
+	printf("\n\n\n\t*\t*\t*\t*\t*\t*\t*\t*");
+	printf("\n\n\n*\t*\t*\t*\t*\t*\t*\t*\t*\t*");
+	printf("\n\n\n\t*\t*\t*\t*\t*\t*\t*\t*");
+	printf("\n\n\n\t\t*\t*\t*\t*\t*\t*");
+	printf("\n\n\n\t\t\t*\t*\t*\t*");
+	printf("\n\n\n\t\t\t\t*\t*");
+	printf("\n\n\n\t\t\t\t    *");
+	
+	
+	//tringle
+	printf("\n\n\n\n\n\n\t\t\t\t    *");
+	printf("\n\n\n\t\t\t\t*\t*");
+	printf("\n\n\n\t\t\t*\t*\t*\t*");
+	printf("\n\n\n\t\t*\t*\t*\t*\t*\t*");
+	printf("\n\n\n\t*\t*\t*\t*\t*\t*\t*\t*");
+	printf("\n\n\n*\t*\t*\t*\t*\t*\t*\t*\t*\t*");
+	
+	
+	
+}
---
+++ Lecuture 1.3/p1.c
@@ -0,0 +1,52 @@
+#include<stdio.h>
+
+main(){
+	
+	//sathiyo
+	printf("*\t\t\t*\t*\t*\t*\n\n\n");
+	printf("*\t\t\t*\n\n\n");
+	printf("*\t\t\t*\n\n\n");
+	printf("*\t\t\t*\n\n\n");
+	printf("*\t*\t*\t*\t*\t*\t*\n\n\n");
+	printf("\t\t\t*\t\t\t*\n\n\n");
+	printf("\t\t\t*\t\t\t*\n\n\n");
+	printf("\t\t\t*\t\t\t*\n\n\n");
+	printf("*\t*\t*\t*\t\t\t*\n\n\n");
+	
+	//tringle
+	printf("\n\n\n\t\t\t\t\t*");
+	printf("\n\n\n\t\t\t\t*\t*\t*");
+	printf("\n\n\n\t\t\t*\t*\t*\t*\t*");
+	printf("\n\n\n\t\t*\t*\t*\t*\t*\t*\t*");
+	printf("\n\n\n\t*\t*\t*\t*\t*\t*\t*\t*\t*");
+	
+	//cross
+	printf("\n\n\n\n\n\n\n\t\t\t*\t\t\t\t\t*");
+	printf("\n\n\n\t\t\t\t*\t\t\t*");
+	printf("\n\n\n\t\t\t\t\t*\t*");
+	printf("\n\n\n\t\t\t\t\t    *");
+	printf("\n\n\n\t\t\t\t\t*\t*");
+	printf("\n\n\n\t\t\t\t*\t\t\t*");
+	printf("\n\n\n\t\t\t*\t\t\t\t\t*");
+	
+	//teda tringal
+	printf("\n\n\n\n\n/n\n\n\t*\t*\t*\t*\t*");
+	printf("\n\n\n\t\t*\t*\t*\t*");
+	printf("\n\n\n\t\t\t*\t*\t*");
+	printf("\n\n\n\t\t\t\t*\t*");
+	printf("\n\n\n\t\t\t\t\t*");
+	printf("\n\n\n\t\t\t\t*\t*");
+	printf("\n\n\n\t\t\t*\t*\t*");
+	printf("\n\n\n\t\t*\t*\t*\t*");
+	printf("\n\n\n\t*\t*\t*\t*\t*");
+	
+}
+
+
+
+
+
+
+
+
+
